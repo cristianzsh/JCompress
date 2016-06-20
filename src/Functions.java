240df209a1a83cfa52c8c0b43756b5326fbcc057
cristian-henrique <cristianmsbr@gmail.com>
2016-06-20 20:13:26 -0300

    Initial commit

---
+++ src/Functions.java
@@ -0,0 +1,73 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;	
+import java.util.zip.ZipFile;
+
+/**
+* JCompress 0.0.1
+* @author Cristian Henrique (cristianmsbr@gmail.com)
+*/
+
+public class Functions {
+	private byte[] buffer = new byte[1024];
+
+	public void compress(String file, String destination) throws Exception {
+		FileInputStream in = new FileInputStream(file);
+		ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(destination));
+		zos.putNextEntry(new ZipEntry(new File(file).getName()));
+
+		int len;
+		while ((len = in.read(buffer)) > 0) {
+			zos.write(buffer, 0, len);
+		}
+		zos.close();
+		in.close();
+	}
+
+	public void unzip(String file, String destination) throws Exception {
+		File dest = new File(destination);
+		if (!dest.exists()) {
+			dest.mkdir();
+		}
+
+		ZipInputStream zis = new ZipInputStream(new FileInputStream(file));
+		ZipEntry ze = zis.getNextEntry();
+
+		while (ze != null) {
+			File f = new File(destination + File.separator + ze.getName());
+			System.out.println("Unzip: " + f.getAbsoluteFile());
+			new File(f.getParent()).mkdirs();
+
+			FileOutputStream fos = new FileOutputStream(f);
+			int len;
+			while ((len = zis.read(buffer)) > 0) {
+				fos.write(buffer, 0, len);
+			}
+
+			fos.close();
+			ze = zis.getNextEntry();
+		}
+		zis.closeEntry();
+		zis.close();
+	}
+
+	public void listFiles(String file) throws Exception {
+		ZipFile f = new ZipFile(file);
+
+		Enumeration<? extends ZipEntry> e = f.entries();
+
+		while (e.hasMoreElements()) {
+			ZipEntry entry = e.nextElement();
+			System.out.println(entry.getName());
+		}
+		f.close();
+	}
+
+	public void help() {
+		System.out.println("Usage: JCompress -[flag] [file] [destination]\n\nFlags:\n\t-c --> Compress file\n\t-u --> Unzip file\n\t-l --> List files\n\t-h --> Help");
+	}
+}
---
+++ src/JCompress.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.List;
+
+/**
+* JCompress 0.0.1
+* @author Cristian Henrique (cristianmsbr@gmail.com)
+*/
+
+public class JCompress {
+	private static Functions f = new Functions();
+
+	public static void main (String[] args) {
+		if (args.length == 0) {
+			System.out
+				.println("JCompress 0.0.1\nDeveloped by Cristian Henrique (cristianmsbr@gmail.com)\n\nUsage: JCompress -[flag] [file] [destination]\nType [JCompress -h] for help");
+			return;
+		}
+
+		try {
+			switch (args[0]) {
+				case "-c":
+					f.compress(args[1], args[2]);
+					break;
+				case "-u":
+					f.unzip(args[1], args[2]);
+					break;
+				case "-l":
+					f.listFiles(args[1]);
+					break;
+				case "-h":
+					f.help();
+			}
+		} catch (Exception ex) {
+			System.out.println("Invalid syntax! Type [JCompress -h] for help");
+		}
+	}
+}
